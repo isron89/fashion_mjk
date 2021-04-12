71f3cd7a9915e90299b2be66085abe1100bb67ae
isron <isronid@gmail.com>
2021-04-12 19:53:29 +0700

    Update about.php

--- application/views/about.php
+++ application/views/about.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
 	
-	<p class="text-center"><h1>Fashion Mojokerto</h1></p>
-	<p class="text-center align-middle">Fashion Mojokerto adalah e commerce umkm untuk pedagang asli mojokerto dalam penjualan fashion</p>
+	<p class="text-center"><h1>Ecommerce Fashion Kota Mojokerto</h1></p>
+	<p class="text-center align-middle">Ecommerce Fashion Kota Mojokerto adalah situs jual beli online untuk umkm asli mojokerto dalam penjualan fashion</p>
 
 </div>
